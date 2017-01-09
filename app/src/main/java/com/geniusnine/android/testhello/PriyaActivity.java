bfc397a2897f4a97a975b8fc50b1218330c538b4
sunilpawar9 <sunilpawar@nineinfosys.com>
2017-01-09 18:37:34 +0530

    Sravani Commited Changes

--- app/src/main/java/com/geniusnine/android/testhello/PriyaActivity.java
+++ app/src/main/java/com/geniusnine/android/testhello/PriyaActivity.java
@@ -1,13 +1,24 @@
 package com.geniusnine.android.testhello;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PriyaActivity extends Activity {
-
+Button btnPriya;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_priya);
+        btnPriya=(Button)findViewById(R.id.button4);
+        btnPriya.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PriyaActivity.this, SravaniActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
